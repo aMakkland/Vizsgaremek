bdacc7e1582ddc829f07e743b2ae374ce96a6c71
aMakkland <ogreg00@gmail.com>
2023-04-14 14:12:40 +0200

    All Product felulet kialakitasa

--- resources/views/admin/all_products.blade.php
+++ resources/views/admin/all_products.blade.php
@@ -3,5 +3,35 @@
     All Product- Eshop
 @endsection
 @section('content')
-    Hello All Product Page
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page/</span> All Products</h4>
+        <div class="card">
+            <h5 class="card-header">Avaible All Product Information</h5>
+            <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Id</th>
+                            <th>Product Name</th>
+                            <th>Img</th>
+                            <th>Price</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <tr>
+                            <td>1</td>
+                            <td>Fan</td>
+                            <td></td>
+                            <td>100</td>
+                            <td>
+                                <a href="" class="btn btn-primary">Edit</a>
+                                <a href="" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
