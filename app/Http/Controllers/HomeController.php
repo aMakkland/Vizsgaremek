76400c609d8a88ee49d4dd52d450c47b965281c5
aMakkland <ogreg00@gmail.com>
2023-05-02 10:53:44 +0200

    Ossze termek megjelenitese a kezdolapon

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function Index()
     {
-        return view('user_template.home');
+        $all_products = Products::latest()->get();
+        return view('user_template.home', compact ('all_products'));
     }
 }
--- resources/views/user_template/layouts/template.blade.php
+++ resources/views/user_template/layouts/template.blade.php
@@ -46,6 +46,8 @@
     <link rel="stylesheet"
         href="{{ asset('home/https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css') }}"
         media="screen">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
